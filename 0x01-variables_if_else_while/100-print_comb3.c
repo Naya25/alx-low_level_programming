0252f154ac20db66b1d0636be89b229826b63011
Naya25 <christendy.eu@gmail.com>
2023-02-17 17:23:31 -0800

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 10 to 89, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = '0'; digit1 <= '8'; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
+		{
+			if (digit2 != digit1)
+			{
+			putchar(digit1);
+			putchar(digit2);
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
