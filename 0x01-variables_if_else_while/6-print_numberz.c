556b89f940d15e435ac362a7f2288eb133db1238
Naya25 <christendy.eu@gmail.com>
2023-02-17 17:04:57 -0800

    6print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
