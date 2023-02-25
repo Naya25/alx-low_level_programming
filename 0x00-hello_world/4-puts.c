a5abd5fe4cf4506751bac8b3c271d4215c7688b3
Naya25 <christendy.eu@gmail.com>
2023-02-25 09:13:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/** 
-* main - Entry point 
-* * Return: Always 0 (Success) 
-*/
-int main(void) {
+/**
+ *main-	entry point of a program that uses pute
+**Return:0
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
